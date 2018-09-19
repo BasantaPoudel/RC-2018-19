fa9b3fcc2e29408005ec25995585986d116cb1c1
Basanta <basanta.poudel@tecnico.ulisboa.pt>
2018-09-19 10:22:23 +0100

    Better Version

--- UDP ex3 and ex4/client.c
+++ UDP ex3 and ex4/client.c
@@ -6,32 +6,41 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <arpa/inet.h>
 #define PORT 58000 // Port to be passed in htons
-////
+
+// struct sockaddr_in{
+//   sa_family_t sin_family;
+//   u_int16_t sin_port;
+//   struct in_addr sin_addr;
+// };
+void *memset(void *s, int c, size_t n);
+int socket(int domain, int type, int protocol);
+ssize_t sendto(int s, const void *buf, size_t len, int flags, const struct sockaddr *to, socklen_t token);
+
 int main(){
-int fd, n,i;
+int fd, n;
+struct sockaddr_in addr;
 struct hostent *hostptr;
-struct sockaddr_in serveraddr, clientaddr;
-int addrlen;
-char msg[80], buffer[80];
+// int addrlen;
+// char msg[80], buffer[80];
 ////
 //UDP Client
-fd=socket(AF_INET,SOCK_DGRAM,0);
+fd=socket(AF_INET,SOCK_DGRAM,0);          // UDP SOCKET //
+if(fd==-1)exit(1);
+
 hostptr=gethostbyname("basanta-UX430UQ");
-memset((void*)&serveraddr,(int)'\0', sizeof(serveraddr));
-serveraddr.sin_family = AF_INET;
-serveraddr.sin_addr.s_addr = ((struct in_addr *)
+
+memset((void*)&addr,(int)'\0', sizeof(addr));
+addr.sin_family = AF_INET;
+addr.sin_addr.s_addr = ((struct in_addr *)
       (hostptr->h_addr_list[0]))->s_addr;
-serveraddr.sin_port = htons((u_short)PORT);
-addrlen = sizeof(serveraddr);
+addr.sin_port = htons((u_short)PORT);
+//addrlen = sizeof(addr);
 
 // Sends data to the Server EX 3
-
-sendto(fd, "Hello!\n", 7, 0, (struct sockaddr*)&serveraddr,addrlen);
-//if(n==-1) exit(1);
-
-
+n=sendto(fd, "Hello!\n", 7, 0, (struct sockaddr*)&addr, sizeof(addr));
+if(n==-1) exit(1);
 close(fd);
 exit(0);
-
 }
--- UDP ex3 and ex4/serv.c
+++ UDP ex3 and ex4/serv.c
@@ -1,38 +1,47 @@
-#include <unistd.h>
+#include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h>
+#include <arpa/inet.h>
 #include <string.h>
-#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <unistd.h>
+#include <netdb.h>
+
 #define PORT 58000
 #define MAX 80
 
-int fd;
-struct hostent *hostptr;
-struct sockaddr_in serveraddr, clientaddr;
-int addrlen;
-char msg[MAX],
- buffer[MAX];
 
 int main(void){
+  int fd,n;
+  struct hostent *hostptr;
+  struct sockaddr_in addr;
+  //int addrlen;
+  //char msg[MAX],
+  char buffer[128];
 
 	fd = socket(AF_INET, SOCK_DGRAM, 0);
 
-	memset((void*) &serveraddr, (int) '\0', sizeof(serveraddr));
-	serveraddr.sin_family = AF_INET;
-	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	serveraddr.sin_port = htons((u_short) PORT);
+//	memset((void*) &addr, (int) '\0', sizeof(addr));
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	addr.sin_port = htons((u_short) PORT);
 
-	bind(fd, (struct sockaddr*) &serveraddr, sizeof(serveraddr));
+	bind(fd, (struct sockaddr*) &addr, sizeof(addr));
 
-	addrlen = sizeof(clientaddr);
+	addrlen = sizeof(addr);
 
-  // Ex 4 recvfrom
-  
-	recvfrom(fd, buffer, MAX, 0, (struct sockaddr*) &clientaddr, &addrlen);
-	printf("%s\n", buffer);
-	sendto(fd, buffer, strlen(buffer), 0, (struct sockaddr*) &clientaddr, addrlen);
+  n=recvfrom(fd, buffer, 128, 0, (struct sockaddr*) &addr, sizeof(addr));
+  if(n==-1)exit(1);
+  write(1,"echo: ", 6); //stdout
+  write(1,buffer, n); //stdout
+
+	// printf("%s\n", buffer);
+	// sendto(fd, buffer, strlen(buffer), 0, (struct sockaddr*) &clientaddr, addrlen);
 
 	close(fd);
+  exit(0);
 }
